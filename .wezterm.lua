3bc8bf06a2e9cba9ff779d382eb8b2206244f450
Jay Crawford <jayhcrawford@gmail.com>
2026-04-08 11:52:43 -0700

    adds conditional for startup processes in windows

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,24 +1,54 @@
 local wezterm = require 'wezterm'
 
+
 local config = wezterm.config_builder()
+
+local is_windows = wezterm.target_triple:find("windows") ~= nil
 
 wezterm.on('gui-startup', function(cmd)
   local mux = wezterm.mux
 
-  local tab1, pane1, window = mux.spawn_window(cmd or {})
-  pane1:send_text('nvim\n')
+  local tab1, pane1, window
+
+  if is_windows then
+    tab1, pane1, window = mux.spawn_window({
+      args = { "nvim" },
+    })
+  else
+    tab1, pane1, window = mux.spawn_window(cmd or {})
+    pane1:send_text('nvim\n')
+  end
+
   tab1:set_title("😊 nvim")
 
-  local tab2, pane2 = window:spawn_tab({})
-  pane2:send_text('pwsh\n')
+  local tab2, pane2
+
+  if is_windows then
+    tab2, pane2 = window:spawn_tab({
+      args = { "pwsh.exe" },
+    })
+  else
+    tab2, pane2 = window:spawn_tab({})
+    pane2:send_text('pwsh\n')
+  end
+
   tab2:set_title("🎉 pwsh")
 end)
 
 -- assign values TO config
+local target = wezterm.target_triple
+if target:find("windows") then
+  config.font_size = 12   -- smaller on Windows
+  config.initial_cols = 170
+  config.initial_rows = 40
+else
+  config.font_size = 18   -- larger on macOS/Linux
+  config.initial_cols = 80
+  config.initial_rows = 24
+end
+
 config.window_close_confirmation = 'NeverPrompt'
-config.font_size = 18
-config.initial_cols = 80
-config.initial_rows = 24
+
 config.color_scheme = 'Aci (Gogh)'
 
 return config
